c6cd19140dde26e0080915ccad88ff63e014c374
mynameisandres <andresbarragan@email.arizona.edu>
2017-09-11 17:45:48 -0700

    step2

--- Hello.java
+++ Hello.java
@@ -13,6 +13,7 @@ public class Hello {
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 	println("Lock S-foils in attack position");
+	println("Stabilize your rear deflectors... Watch for enemy fighters");
     }
 
     public static void goldLeader() {
